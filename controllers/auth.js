26cbb3c537e3afbcbe22b3a8ee87c578621e0c1b
Ali <a.hodavand@gmail.com>
2020-04-04 19:01:23 +0430

    initial postLogin and postLogout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
     res.render("auth/login", {
         pageTitle: "Login",
@@ -6,6 +8,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    res.redirect("/");
+    User.findById("5e7bc98eec491e78d24e6ae3")
+        .then((user) => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            res.redirect("/");
+        })
+        .catch((err) => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        console.log(err);
+        res.redirect("/");
+    });
 };
