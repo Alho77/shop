502fd157a3dab35fff0b378b7189f91483512e58
Ali <a.hodavand@gmail.com>
2020-04-05 21:32:11 +0430

    route protecting

--- routes/admin.js
+++ routes/admin.js
@@ -1,21 +1,22 @@
 const express = require("express");
 
 const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is_auth");
 
 const router = express.Router();
 
-router.get("/products", adminController.getProducts);
+router.get("/products", isAuth, adminController.getProducts);
 
 // /admin/add-product => GET
-router.get("/add-product", adminController.getAddProducts);
+router.get("/add-product", isAuth, adminController.getAddProducts);
 
 // /admin/add-product => POST
-router.post("/add-product", adminController.postAddProducts);
+router.post("/add-product", isAuth, adminController.postAddProducts);
 
-router.get("/edit-product/:productId", adminController.getEditProducts);
+router.get("/edit-product/:productId", isAuth, adminController.getEditProducts);
 
-router.post("/edit-product", adminController.postEditProduct);
+router.post("/edit-product", isAuth, adminController.postEditProduct);
 
-router.post("/delete-product", adminController.postDeleteProduct);
+router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
 exports.routes = router;
